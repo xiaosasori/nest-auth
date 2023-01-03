fa540f8fbf700d349c16bca82f6b35428fe80afa
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-01-03 13:27:44 +0700

    setup db and users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { UsersModule } from './users/users.module';
 import { IamModule } from './iam/iam.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule, UsersModule, IamModule],
+  imports: [
+    CoffeesModule,
+    UsersModule,
+    IamModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
